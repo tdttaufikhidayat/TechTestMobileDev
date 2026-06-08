de180bf1bc20c22050e786e3ebdcbbe75615862e
tdttaufikhidayat <tdt.taufikhidayat@gmail.com>
2026-06-08 12:22:30 +0700

    feat: implement ArticlePagingSource for endless scrolling

---
+++ app/src/main/java/com/taufikhidayat/techtestmobiledev/data/paging/ArticlePagingSource.kt
@@ -0,0 +1,46 @@
+package com.taufikhidayat.techtestmobiledev.data.paging
+
+import androidx.paging.PagingSource
+import androidx.paging.PagingState
+import com.taufikhidayat.techtestmobiledev.data.remote.NewsApi
+import com.taufikhidayat.techtestmobiledev.data.remote.dto.ArticleDto
+import retrofit2.HttpException
+import java.io.IOException
+
+class ArticlePagingSource(
+    private val api: NewsApi,
+    private val sourceId: String
+) : PagingSource<Int, ArticleDto>() {
+
+    override fun getRefreshKey(state: PagingState<Int, ArticleDto>): Int? {
+        return state.anchorPosition?.let { anchorPosition ->
+            state.closestPageToPosition(anchorPosition)?.prevKey?.plus(1)
+                ?: state.closestPageToPosition(anchorPosition)?.nextKey?.minus(1)
+        }
+    }
+
+    override suspend fun load(params: LoadParams<Int>): LoadResult<Int, ArticleDto> {
+        val currentPage = params.key ?: 1
+        return try {
+            val response = api.getArticles(
+                sources = sourceId,
+                page = currentPage,
+                pageSize = params.loadSize
+            )
+
+            val articles = response.articles?.filter { it.title != "[Removed]" } ?: emptyList()
+
+            LoadResult.Page(
+                data = articles,
+                prevKey = if (currentPage == 1) null else currentPage - 1,
+                nextKey = if (articles.isEmpty()) null else currentPage + 1
+            )
+        } catch (e: IOException) {
+            LoadResult.Error(e) // Error jaringan (tidak ada internet)
+        } catch (e: HttpException) {
+            LoadResult.Error(e) // Error dari server (misal limit API habis)
+        } catch (e: Exception) {
+            LoadResult.Error(e)
+        }
+    }
+}
