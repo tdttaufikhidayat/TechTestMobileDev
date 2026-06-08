1936027a435008380c0042f12b430b76cf8e8c59
tdttaufikhidayat <tdt.taufikhidayat@gmail.com>
2026-06-08 12:36:06 +0700

    feat: add navigation routes sealed class

---
+++ app/src/main/java/com/taufikhidayat/techtestmobiledev/presentation/navigation/Screen.kt
@@ -0,0 +1,23 @@
+package com.taufikhidayat.techtestmobiledev.presentation.navigation
+
+import java.net.URLEncoder
+import java.nio.charset.StandardCharsets
+
+sealed class Screen(val route: String) {
+    object Source : Screen("source_screen")
+
+    // Melempar ID dan Nama Sumber ke halaman artikel
+    object Article : Screen("article_screen/{sourceId}/{sourceName}") {
+        fun createRoute(sourceId: String, sourceName: String): String {
+            return "article_screen/$sourceId/$sourceName"
+        }
+    }
+
+    // Melempar URL web ke halaman detail (URL harus di-encode agar tidak merusak format route)
+    object Detail : Screen("detail_screen/{url}") {
+        fun createRoute(url: String): String {
+            val encodedUrl = URLEncoder.encode(url, StandardCharsets.UTF_8.toString())
+            return "detail_screen/$encodedUrl"
+        }
+    }
+}
